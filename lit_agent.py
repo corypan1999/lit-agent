ae56997b05a567ce748b91ef9a6518232b8efb54
corypan1999 <78058071+corypan1999@users.noreply.github.com>
2026-09-04 15:10:27 -0500

    Update lit_agent.py
    added a name to priority author

--- lit_agent.py
+++ lit_agent.py
@@ -99,7 +99,7 @@ TIER_INSTRUCTIONS = {
 # ---------------------------------------------------------------------------
 
 PRIORITY_AUTHORS = [
-    "Benjamin L Ebert", "Siddhartha Jaiswal", "Ravindra Majeti", "Vijay G Sankaran", "Dan A Landau", "R Coleman Lindsley"
+    "Benjamin L Ebert", "Siddhartha Jaiswal", "Ravindra Majeti", "Vijay G Sankaran", "Dan A Landau", "R Coleman Lindsley", "Iannis Aifantis"
 ]
 
 PRIORITY_TERMS = [
